44aa39df11131d47cd71558f0885620dae9e22cf
MykolaMatsko <mykola.matsko@gmail.com>
2024-04-22 11:52:02 +0300

    Add project files.

---
+++ Module2.BeforeDI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Module2.BeforeDI;
+using Module2.BeforeDI.Shared;
+using Module2.BeforeDI.Source;
+using Module2.BeforeDI.Target;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var configuration = new Configuration();
+
+var priceParser = new PriceParser();
+var productSource = new ProductSource(configuration, priceParser);
+
+var productFormatter = new ProductFormatter();
+var productTarget = new ProductTarget(configuration, productFormatter);
+
+var productImporter = new ProductImporter(productSource, productTarget);
+productImporter.Run();
